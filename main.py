fe7b6e08cd404a2ec9de245066bd68472bb9f6ab
Ifeoluwa <ifeolufaro@gmail.com>
2025-05-27 01:04:24 +0100

    second commit

--- main.py
+++ main.py
@@ -137,5 +137,5 @@ def login_user(user: LoginUser):
     }
   
 
-if __name__ == '__main__':
-  uvicorn.run('main:app', reload=True)
+# if __name__ == '__main__':
+#   uvicorn.run('main:app', reload=True)
